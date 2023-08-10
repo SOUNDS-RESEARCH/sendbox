092efb86aa68c4e4234e9a46879e76def55129f5
p-didier <paul.didier@esat.kuleuven.be>
2023-08-10 15:06:44 +0200

    Test read opus

---
+++ test_read_opus.py
@@ -0,0 +1,19 @@
+# Purpose of script:
+# Test the read_opus function.
+#
+# (c) Paul Didier, SOUNDS ETN, KU Leuven ESAT STADIUS
+
+import sys
+
+FILENAME = 'output.opus'
+
+def main():
+    """Main function (called by default when running script)."""
+    read_opus(FILENAME)
+
+def read_opus(fileName):
+    """Read an opus file and return the data."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
