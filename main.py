879a6d348e647598c2fed3b63333f96667989b75
MohammadBokaei <104490652+MohammadBokaei@users.noreply.github.com>
2023-08-10 14:57:43 +0200

    opus encoder

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import numpy as np
+import os
+
+
+
+def call_opus_encoder(name):
+    input = 'opusenc --bitrate 160 ' + name + ' ' + 'output.opus'
+    print(input)
+    enocoding_check = os.system(input)
+
+    if enocoding_check == 1:
+        raise Exception("Encoding did not go correct !!")
+    
+    
+
+
+
+if __name__ == '__main__':
+    name = 'test.flac'
+    call_opus_encoder(name)
