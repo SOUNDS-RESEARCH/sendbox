b662ad268d537afc0f8e0d8d794125d210e42ff7
p-didier <paul.didier@esat.kuleuven.be>
2023-08-17 15:54:27 +0200

    Complete system v1

--- commpy_trial_analog.py
+++ commpy_trial_analog.py
@@ -11,76 +11,143 @@ import commpy.channels as chan
 import commpy.links as lk
 import commpy.modulation as mod
 
-def analog_modeller(bits):
-    
-    # =============================================================================
-    # Convolutional Code 1: G(D) = [1+D^2, 1+D+D^2]
-    # Standard code with rate 1/2
-    # =============================================================================
-    # Number of delay elements in the convolutional encoder
+
+def encoding(bits):
+
     memory = np.array(2, ndmin=1)
-    
-    # Generator matrix
     g_matrix = np.array((0o5, 0o7), ndmin=2)
-    
     # Create trellis data structure
     trellis = cc.Trellis(memory, g_matrix)
+
+    encodedBits = cc.conv_encode(bits, trellis, 'cont')
+
+    return encodedBits, trellis
+
     
-    # ==================================================================================================
-    # Complete example using Commpy features and compare hard and soft demodulation. Example with code 1
-    # ==================================================================================================
-    
-    # Modem : QPSK
-    modem = mod.QAMModem(4)
-    
+# Decoder function
+def decoder_hard(msg, trellis):
+    return cc.viterbi_decode(msg, trellis)
+
+
+# Decoder function
+def decoder_soft(msg, trellis):
+    return cc.viterbi_decode(msg, trellis, decoding_type='soft')
+
+
+SIZE_OF_QAM_CONSTELLATION = 16
+NOISE_STD = 1.6  # TODO: the SNR depends on SIZE_OF_QAM_CONSTELLATION - fix that
+
+def analog_modeller(
+        bits,
+        noiseStd=NOISE_STD,
+        sizeOfQamConstellation=SIZE_OF_QAM_CONSTELLATION,
+        plot=False
+    ):
+    """Modulate and demodulate bits using analog modulation."""
+
+    # Encoding
+    print('Encoding bits (channel)...')
+    encodedBits, trellis = encoding(bits)
+
+    # Modulation
+    print('Modulating bits...')
+    modem = mod.QAMModem(sizeOfQamConstellation)
+    modulatedBits = modem.modulate(encodedBits)
+
     # AWGN channel
-    channels = chan.SISOFlatChannel(None, (1 + 0j, 0j))
+    print('Propagating bits through AWGN channel...')
+    channels = chan.SISOFlatChannel(
+        noise_std=noiseStd,
+        fading_param=(1 + 0j, 0j)
+    )
+    receivedSignal = channels.propagate(modulatedBits)
+
+    # Demodulation
+    print('Demodulating bits...')
+    demodulatedBits = modem.demodulate(receivedSignal, 'hard')
+    # demodulatedBits = modem.demodulate(out, 'soft')   # THROWS ERROR FOR SOME REASON
+
+    # Decoding
+    print('Decoding bits (channel) [BE PATIENT!]...')
+    decodedBits = decoder_hard(demodulatedBits, trellis)
+    # decodedBits = decoder_soft(encodedBits)   # LESS ACCURATE
     
-    # SNR range to test
-    SNRs = np.arange(0, 6) + 10 * math.log10(modem.num_bits_symbol)
+    if plot:
+        fig, axes = plt.subplots(2, 1)
+        fig.set_size_inches(8.5, 3.5)
+        #
+        axes[0].plot(bits, '.-', label='Original bits')
+        axes[0].plot(demodulatedBits, '.--', label='Demodulated bits')
+        axes[0].set_ylabel('Bit value')
+        axes[0].set_title(f'DEMODULATION OUTPUT - Noise level {noiseStd} - {sizeOfQamConstellation}-QAM')
+        axes[0].legend()
+        #
+        axes[1].plot(bits, '.-', label='Original bits')
+        axes[1].plot(decodedBits, '.--', label='Decoded bits')
+        axes[1].set_ylabel('Bit value')
+        axes[1].set_title(f'DECODER OUTPUT - Noise level {noiseStd} - {sizeOfQamConstellation}-QAM')
+        axes[1].legend()
+        #
+        fig.tight_layout()
+        plt.show(block=False)
+
+    return decodedBits
+
+    # ==================================================================================================
+    # # Complete example using Commpy features and compare hard and soft demodulation. Example with code 1
+    # # ==================================================================================================
     
-    # Modulation function
-    def modulate(bits):
-        return modem.modulate(cc.conv_encode(bits, trellis, 'cont'))
+    # # Modem : QPSK
+    # modem = mod.QAMModem(4)
+    
+    # # AWGN channel
+    # channels = chan.SISOFlatChannel(None, (1 + 0j, 0j))
+    
+    # # SNR range to test
+    # SNRs = np.arange(0, 6) + 10 * math.log10(modem.num_bits_symbol)
+    
+    # # Modulation function
+    # def modulate(bits):
+    #     return modem.modulate(cc.conv_encode(bits, trellis, 'cont'))
     
     
-    # Receiver function (no process required as there are no fading)
-    def receiver_hard(y, h, constellation, noise_var):
-        return modem.demodulate(y, 'hard')
+    # # Receiver function (no process required as there are no fading)
+    # def receiver_hard(y, h, constellation, noise_var):
+    #     return modem.demodulate(y, 'hard')
     
     
-    # Receiver function (no process required as there are no fading)
-    def receiver_soft(y, h, constellation, noise_var):
-        return modem.demodulate(y, 'soft', noise_var)
+    # # Receiver function (no process required as there are no fading)
+    # def receiver_soft(y, h, constellation, noise_var):
+    #     return modem.demodulate(y, 'soft', noise_var)
     
     
-    # Decoder function
-    def decoder_hard(msg):
-        return cc.viterbi_decode(msg, trellis)
+    # # Decoder function
+    # # def decoder_hard(msg):
+    # #     return cc.viterbi_decode(msg, trellis)
     
     
-    # Decoder function
-    def decoder_soft(msg):
-        return cc.viterbi_decode(msg, trellis, decoding_type='soft')
+    # # # Decoder function
+    # # def decoder_soft(msg):
+    # #     return cc.viterbi_decode(msg, trellis, decoding_type='soft')
     
-    # Build model from parameters
-    code_rate = trellis.k / trellis.n
-    model_hard = lk.LinkModel(modulate, channels, receiver_hard,
-                              modem.num_bits_symbol, modem.constellation, modem.Es,
-                              decoder_hard, code_rate)
-    model_soft = lk.LinkModel(modulate, channels, receiver_soft,
-                              modem.num_bits_symbol, modem.constellation, modem.Es,
-                              decoder_soft, code_rate)
+    # # Build model from parameters
+    # code_rate = trellis.k / trellis.n
+    # model_hard = lk.LinkModel(modulate, channels, receiver_hard,
+    #                           modem.num_bits_symbol, modem.constellation, modem.Es,
+    #                           decoder_hard, code_rate)
+    # model_soft = lk.LinkModel(modulate, channels, receiver_soft,
+    #                           modem.num_bits_symbol, modem.constellation, modem.Es,
+    #                           decoder_soft, code_rate)
     
-    # Test
-    BERs_hard = model_hard.link_performance(SNRs, 10000, 600, 5000, code_rate)
-    BERs_soft = model_soft.link_performance(SNRs, 10000, 600, 5000, code_rate)
-    plt.semilogy(SNRs, BERs_hard, 'o-', SNRs, BERs_soft, 'o-')
-    plt.grid()
-    plt.xlabel('Signal to Noise Ration (dB)')
-    plt.ylabel('Bit Error Rate')
-    plt.legend(('Hard demodulation', 'Soft demodulation'))
-    plt.show()
+    # # Test
+    # BERs_hard = model_hard.link_performance(SNRs, 10000, 600, 5000, code_rate)
+    # BERs_soft = model_soft.link_performance(SNRs, 10000, 600, 5000, code_rate)
+    # plt.semilogy(SNRs, BERs_hard, 'o-', SNRs, BERs_soft, 'o-')
+    # plt.grid()
+    # plt.xlabel('Signal to Noise Ration (dB)')
+    # plt.ylabel('Bit Error Rate')
+    # plt.legend(('Hard demodulation', 'Soft demodulation'))
+    # plt.show()
     
     
             
--- test_read_opus.py
+++ test_read_opus.py
@@ -6,25 +6,61 @@
 import sys
 import numpy as np
 from bitstring import BitArray
+import commpy_trial_digital as ctd
+import commpy_trial_analog as cta
+import numpy as np
+import os
 
-FILENAME = 'output.opus'
+# Global variables
+from sys import platform as PLATFORM
+OPUS_DIR = 'opus'
+
+FILENAME = 'test.flac'
+SEED = 0
 
 def main():
     """Main function (called by default when running script)."""
+
+    np.random.seed(SEED)
+
+    # call_opus_encoder(
+    #     wav_filename='test.flac',
+    #     opus_out_name=opus_out_name,
+    # )
+    
+    print('Calling OPUS decoder...')
+    call_opus_decoder(
+        opus_filename='output.opus',
+        wav_out_name='output2.wav'
+    )
+
+
+
     data = read_opus(FILENAME)
 
     # Convert to BitArray object
     bitArray = BitArray(bytes=data)
     # Extract actual bits as np.ndarray
     bitsList = np.array([int(b) for b in bitArray.bin])
+
+    bitsList = bitsList[:8**5]
+
     # Modify bits as you want
-    bitsListModified = bitsList  # whatever CommPy does
+    decodedBits = cta.analog_modeller(bitsList, noiseStd=0.0, plot=False)
     # Go back to bytes via BitArray object
-    backToBytes = BitArray(bitsListModified).bytes
+    backToBytes = BitArray(decodedBits).bytes
 
-    # print(data == backToBytes)
+    # Write OPUS file
+    print('Writing OPUS file...')
+    write_opus('output2.opus', backToBytes)
 
-    stop = 1 
+    # Call OPUS decoder
+    print('Calling OPUS decoder...')
+    call_opus_decoder(
+        opus_filename='output.opus',
+        wav_out_name='output2.wav'
+    )
+
 
 def read_opus(fileName):
     """Read an opus file and return the data."""
@@ -32,6 +68,63 @@ def read_opus(fileName):
     data = f.read()
     f.close()
     return data
+
+
+def write_opus(fileName, data):
+    """Writes an opus file."""
+    f = open(fileName, 'wb')
+    f.write(data)
+    f.close()
+
+
+def call_opus_encoder(
+        wav_filename,
+        opus_out_name,
+        encoder_name='opusenc',
+        bitrate=160
+    ):
+    """Call the opus encoder with the given parameters."""
+
+    fullPathOpus = build_opus_path(encoder_name)
+
+    encoding_check = os.system(
+        f'{fullPathOpus} --bitrate {bitrate} {wav_filename} {opus_out_name}'
+    )
+
+    if encoding_check == 1:
+        raise Exception("Encoding failed !!")
+    
+
+def call_opus_decoder(
+        opus_filename,
+        wav_out_name,
+        decoder_name='opusdec'
+    ):
+    """Call the opus decoder with the given parameters."""
+
+    fullPathOpus = build_opus_path(decoder_name)
+
+    decoding_check = os.system(
+        f'{fullPathOpus} {opus_filename} {wav_out_name}'
+    )
+
+    if decoding_check == 1:
+        raise Exception("Decoding failed !!")
+
+
+def build_opus_path(encoder_name):
+    """Build the path to the opus encoder/decoder, 
+    depending on the platform."""
+
+    fullPathOpus = f'{OPUS_DIR}'
+    if not 'darwin' in PLATFORM:
+        sep = '\\'
+        fullPathOpus += f'{sep}win'
+        encoder_name += '_win'
+    else:
+        sep = '/'
+    fullPathOpus += f'{sep}{encoder_name}'
+    return fullPathOpus
 
 if __name__ == '__main__':
     sys.exit(main())
