d07ac9ac6ef785015ae8b04b297a5df29495f2d4
Paul Didier <paul.didier@kuleuven.be>
2023-10-12 10:43:56 +0200

    cleanup

--- test_read_opus.py
+++ test_read_opus.py
@@ -1,26 +1,29 @@
 # Purpose of script:
-# Test the read_opus function.
+# Encodes a WAV file to OPUS, apply some channel-related disturbances
+# on the bytes via a channel model, then decodes it back to WAV.
 #
-# (c) Paul Didier, Mohammad Bokaei, José Miguel Cadavid Tobón, SOUNDS ETN
+# (c) Paul Didier, Mohammad Bokaei, José Miguel Cadavid Tobón (SOUNDS ETN 2023)
 
 import os
 import sys
-import pydub  # requires ffmpeg
+import pydub  # requires `ffmpeg`
 import numpy as np
 from pathlib import Path
 from bitstring import BitArray
 import commpy_trial_analog as cta
-
 from sys import platform as PLATFORM
-OPUS_DIRECTORY = 'opus'
-INPUT_WAV_FILENAME = 'test_short.wav'
-SEED = 0
-SNR = 10  # AWGN channel SNR [dB]
-SIZE_OF_QAM_CONSTELLATION = 64
-FRAME_SIZE = 2.5 #ms
-BITRATE = 32 # kb
 
-AUDIO_CODEC = 'opus'  # 'opus' or something else
+# Global variables (change as you want)
+OPUS_DIRECTORY = 'opus'  # directory where opus encoder/decoder is located
+INPUT_WAV_FILENAME = 'test_short.wav'  # input WAV file
+SEED = 0  # seed for reproducibility
+SNR = 10  # AWGN channel SNR [dB]
+SIZE_OF_QAM_CONSTELLATION = 64  # 4, 16, 64, 256, 1024, 4096, 16384 (cf. `cta.analog_modeller`)
+FRAME_SIZE = 2.5  # OPUS encoding frame size [ms]
+BITRATE = 32  # OPUS encoding bitrate [kbits/s]
+
+# Probably don't change these
+AUDIO_CODEC = 'opus'  # 'opus' or something else (e.g., 'mp3')
 
 def main(inWavFilename=INPUT_WAV_FILENAME):
     """Main function (called by default when running script)."""
@@ -28,27 +31,27 @@ def main(inWavFilename=INPUT_WAV_FILENAME):
     # Set seed for reproducibility
     np.random.seed(SEED)
 
+    # Encode WAV file to OPUS
     print(f'Calling {AUDIO_CODEC.capitalize()} encoder...')
     encOutFilename = Path(inWavFilename).stem + f'_to_{AUDIO_CODEC}.{AUDIO_CODEC}'
     if (Path(encOutFilename).exists()):
         os.remove(encOutFilename)
     os.system(f'ffmpeg -i {inWavFilename} -frame_duration {FRAME_SIZE} -b:a {BITRATE}K -vbr off {encOutFilename}')
     
+    # Read OPUS file
     print(f'Reading {AUDIO_CODEC.capitalize()} file...')
     audioEncoded: pydub.AudioSegment = pydub.AudioSegment.from_file(
         encOutFilename,
-        # format=AUDIO_CODEC,
         codec = 'opus'
     )
     data = audioEncoded.raw_data
-
-    # Convert to BitArray object
+    # Convert to `BitArray` object
     print('Converting to BitArray object...')
     bitArray = BitArray(bytes=data)
-    # Extract actual bits as np.ndarray
+    # Extract actual bits as `np.ndarray`
     bitsList = np.array([int(b) for b in bitArray.bin])
 
-    # Modify bits as you want
+    # Modify bits as we want
     print('----- Modifying bits (channel effect) -----')
     decodedBits = cta.analog_modeller(
         bitsList,
@@ -59,9 +62,8 @@ def main(inWavFilename=INPUT_WAV_FILENAME):
     backToBytes = BitArray(decodedBits).bytes
     print('----- Done modifying bits -----')
 
+    # Write modified bits to WAV file
     backToWavFilename = f'{Path(inWavFilename).stem}_{AUDIO_CODEC}_codec_{SNR}dB_noise_QAM{SIZE_OF_QAM_CONSTELLATION}.wav'
-
-    # Write WAV file
     print('Writing WAV file...')
     audioForOutput = pydub.AudioSegment(
         data=backToBytes,
@@ -97,16 +99,13 @@ def call_opus_encoder(
         bitrate=160
     ):
     """Call the opus encoder with the given parameters."""
-
     fullPathOpus = build_opus_path(encoder_name)
-
     encoding_check = os.system(
         f'{fullPathOpus} --bitrate {bitrate} {wav_filename} {opus_out_name}'
     )
-
     if encoding_check == 1:
         raise Exception("Encoding failed !!")
-    
+
 
 def call_opus_decoder(
         opus_filename,
@@ -114,13 +113,10 @@ def call_opus_decoder(
         decoder_name='opusdec'
     ):
     """Call the opus decoder with the given parameters."""
-
     fullPathOpus = build_opus_path(decoder_name)
-
     decoding_check = os.system(
         f'{fullPathOpus} {opus_filename} {wav_out_name}'
     )
-
     if decoding_check == 1:
         raise Exception("Decoding failed !!")
 
@@ -128,16 +124,15 @@ def call_opus_decoder(
 def build_opus_path(encoder_name, opus_dir=OPUS_DIRECTORY):
     """Build the path to the opus encoder/decoder, 
     depending on the platform."""
-
     fullPathOpus = opus_dir
     if not 'darwin' in PLATFORM:
         sep = '\\'
         fullPathOpus += f'{sep}win'
-        # encoder_name += '_win'
     else:
         sep = '/'
     fullPathOpus += f'{sep}{encoder_name}'
     return fullPathOpus
+
 
 if __name__ == '__main__':
     sys.exit(main())
